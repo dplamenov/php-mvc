f0cb550624f8327e66dd7396c12d447e4d7950e3
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-01-13 14:07:09 +0200

    Database insert

--- controllers/Welcome.php
+++ controllers/Welcome.php
@@ -6,8 +6,6 @@ class Welcome
 {
     public function showForm()
     {
-        $database = \Application\Database::init();
-        $database->insert('INSERT INTO `pages` (`page_title`, `page_body`) VALUES (?, ?)' , ['title', 'body']);
         return Base::View('welcome');
     }
 
