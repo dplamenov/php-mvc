6fef390ea88ba4e880cbf395b45c05dd206e2ad2
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-01-17 17:02:22 +0200

    validate trait

--- system/Validate.php
+++ system/Validate.php
@@ -17,6 +17,7 @@ trait Validate
                 unset($validate_rules[$name]);
             }
         }
+        echo '<pre>' . print_r($request, true) . '</pre>';
         foreach ($request_ as $key => $value) {
             $data[$key]['value'] = $value;
             $data[$key]['rules'] = $validate_rules[$key];
@@ -26,6 +27,13 @@ trait Validate
             $data[$k]['rules'] = $rules;
         }
 
-
+        foreach ($data as $k => $v) {
+            $rules = $data[$k]['rules'];
+            $data[$k]['rules'] = array_map(function ($element) {
+                $element = explode(':', $element);
+                return $element;
+            }, $rules);
+        }
+        
     }
 }
