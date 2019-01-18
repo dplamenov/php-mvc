adb86108b46062ef5d611067db33b78a15d57a5d
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-01-18 15:02:56 +0200

    add 2 method in session class

--- system/Session.php
+++ system/Session.php
@@ -1,11 +1,5 @@
 <?php
-/**
- * Project: php-mvc.
- * File: Session.php
- * Developer: dplamenov@icloud.com
- * Date: 18.1.2019 г.
- * Time: 14:57
- */
+
 
 namespace Application;
 
@@ -19,6 +13,6 @@ class Session
 
     public function get()
     {
-        
+
     }
 }
