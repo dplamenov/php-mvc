e7629480053a142f014bcfecd14f666dc8377881
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-01-18 18:00:30 +0200

    bug fix

--- controllers/Welcome.php
+++ controllers/Welcome.php
@@ -9,8 +9,8 @@ class Welcome extends Controller
     public function showForm(\Application\Request $request)
     {
         $request->session()->put('num', $request->session()->get('num') + 1);
-        echo $request->session()->get('num');
-        return Base::View('welcome');
+        $num = $request->session()->get('num');
+        return Base::View('welcome', ['num' => $num]);
     }
 
     public function storeData(\Application\Request $request)
