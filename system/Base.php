74db42f89bcbc6db6da0afb20b5a0fab6403e4d5
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-01-21 12:40:01 +0200

    delete unnecessary method

--- system/Base.php
+++ system/Base.php
@@ -29,17 +29,7 @@ class Base
         return $msg;
     }
 
-    private static function get_string_between($string, $start, $end)
-    {
-        $string = ' ' . $string;
-        $ini = strpos($string, $start);
-        if ($ini == 0) return '';
-        $ini += strlen($start);
-        $len = strpos($string, $end, $ini) - $ini;
-        return substr($string, $ini, $len);
-    }
-
-
+  
     public static function redirect($to)
     {
         header("Location: " . $_SERVER['REQUEST_URI'] . ltrim($to, '/'));
