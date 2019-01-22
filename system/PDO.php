c07ec14454847980d71fd685aed235e1c5ad21b1
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-01-22 10:42:20 +0200

    PDO add method init

--- system/PDO.php
+++ system/PDO.php
@@ -12,12 +12,17 @@ namespace Application;
 
 class PDO
 {
+    private $pdo;
+    private static $instance = null;
     protected function __construct($dsn, $user, $password)
     {
+        $this->pdo = new PDO($dsn, $user, $password);
     }
 
     public static function init($dsn, $user, $password)
     {
-        
+        if (self::$instance == null) {
+            self::$instance = new self($dsn, $user, $password);
+        }
     }
 }
